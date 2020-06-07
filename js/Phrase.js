0a2aebbd63392a80df715cdbdb93a7d7cc76fc8d
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-06-07 17:14:45 -0400

    begin addPhraseToDisplay() function
    found phrase in DOM

--- js/Phrase.js
+++ js/Phrase.js
@@ -10,12 +10,31 @@
 
     //adds letter placeholders to the display when the game starts. Each letter is presented by an empty box, one li element for each letter.
     addPhraseToDisplay() {
-        return 
+        let arrPhrase = this.phrase.split(''); 
+        arrPhrase.forEach(letter => console.log(letter));
+    
+
+        /*<div id="phrase" class="section">
+    <ul>
+        <li class="hide letter h">h</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter w">w</li>
+        <li class="space"> </li>
+        <li class="hide letter a">a</li>
+        <li class="hide letter r">r</li>
+        <li class="hide letter e">e</li>
+        <li class="space"> </li>
+        <li class="hide letter y">y</li>
+        <li class="hide letter o">o</li>
+        <li class="hide letter u">u</li>
+    </ul>
+</div>
+    */
     }
 
     //checks to see if the letter selected by the player matches a letter in the phrase.
     checkLetter() {
-        return
+        
     }
 
     //reveals the letter(s) on the board that matches the player's selection.
@@ -29,3 +48,4 @@
 
 console.log(ph)
 console.log(ph.phrase)
+ph.addPhraseToDisplay();
