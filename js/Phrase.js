4335a01bf5a57bad0d3b1435bfcd10a23ee610c6
Katherine Jablonski <31462880+KJablonski08@users.noreply.github.com>
2020-06-07 16:31:38 -0400

    created game class in game.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -3,9 +3,26 @@
  * Phrase.js */
 
  class Phrase {
+
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
 
+    //adds letter placeholders to the display when the game starts. Each letter is presented by an empty box, one li element for each letter.
+    addPhraseToDisplay() {
+        return 
+    }
+
+    //checks to see if the letter selected by the player matches a letter in the phrase.
+    checkLetter() {
+        return
+    }
+
+    //reveals the letter(s) on the board that matches the player's selection.
+    showMatchedLetter() {
+
+    }
+
  }
+
 
